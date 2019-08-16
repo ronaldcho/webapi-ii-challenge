7fc3a3743d4afb3c672269de469b19f40a3062c6
ronaldcho@gmail.com <ronaldcho@gmail.com>
2019-08-15 21:48:37 -0700

    create server.js

---
+++ index.js
@@ -0,0 +1,7 @@
+const server = require('./server.js');
+
+// const port = 5000;
+
+server.listen(5000, () => {
+	console.log(`\n ****** Server is listening on http://localhost:5000 *********\n`);
+})
